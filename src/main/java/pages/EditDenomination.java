08ddfcc92cb96f02c22d25991aa8cdf67ef45440
shilpa.venkatappa <shilpa.tv@rumango.com>
2022-06-03 14:05:24 +0530

    test commit

--- src/main/java/pages/EditDenomination.java
+++ src/main/java/pages/EditDenomination.java
@@ -2,21 +2,26 @@ package pages;
 
 import base.IcustSpecificMethods;
 
-public class EditDenomination extends IcustSpecificMethods{
-	
-		public EditDenomination editDenomCode() {
-			enterValue(locateElement("xpath", props.getProperty("EditDenomination.EditDenomcode.Xpath")), props.getProperty("EditDenomcode"));
-			return this;
-	}
-		public EditDenomination editDenomDesc() {
-			enterValue(locateElement("xpath", props.getProperty("EditDenomination.EditDenomdesc.Xpath")), props.getProperty("EditDenomdesc"));
-			return this;
-		}
-		public EditDenomination clickSave() {
-			clickElement(locateElement("xpath", props.getProperty("EditDenomination.save.Xpath")));
-			return this;
-		}
-	
-	
+/**
+ * @author Shilpa Venkatappa
+ *
+ */
+public class EditDenomination extends IcustSpecificMethods {
 
+	public EditDenomination editDenomCode() {
+		enterValue(locateElement("xpath", props.getProperty("EditDenomination.EditDenomcode.Xpath")),
+				props.getProperty("EditDenomcode"));
+		return this;
+	}
+
+	public EditDenomination editDenomDesc() {
+		enterValue(locateElement("xpath", props.getProperty("EditDenomination.EditDenomdesc.Xpath")),
+				props.getProperty("EditDenomdesc"));
+		return this;
+	}
+
+	public EditDenomination clickSave() {
+		clickElement(locateElement("xpath", props.getProperty("EditDenomination.save.Xpath")));
+		return this;
+	}
 }
--- src/main/java/pages/IcustAddBaseDenominationmaintenance.java
+++ src/main/java/pages/IcustAddBaseDenominationmaintenance.java
@@ -1,37 +1,40 @@
 package pages;
 
-import org.openqa.selenium.WebElement;
-
 import base.IcustSpecificMethods;
 
-public class IcustAddBaseDenominationmaintenance extends IcustSpecificMethods{
+public class IcustAddBaseDenominationmaintenance extends IcustSpecificMethods {
 
-	public IcustAddBaseDenominationmaintenance selectBaseDenominationMaintenance() 
-	{
+	public IcustAddBaseDenominationmaintenance selectBaseDenominationMaintenance() {
 		clickElement(locateElement("xpath", props.getProperty("Icust.Maintainence.BaseDenominationMaintenance.Xpath")));
 		wait(5000);
-		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.BaseDenominationMaintenance.AddNew.Xpath")));
+		clickElement(
+				locateElement("xpath", props.getProperty("IcustMaintenance.BaseDenominationMaintenance.AddNew.Xpath")));
 		return this;
 	}
-	
+
 	public IcustAddBaseDenominationmaintenance enterDenomination() {
-		enterValue(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.Bank.AddNew.Denomination.Xpath")), props.getProperty("BankName"));
+		enterValue(
+				locateElement("xpath",
+						props.getProperty("IcustMaintenance.Maintenance.Bank.AddNew.Denomination.Xpath")),
+				props.getProperty("BankName"));
 		return this;
 	}
-	
+
 	public IcustAddBaseDenominationmaintenance selectCurrency() {
-		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.Xpath")));
+		clickElement(locateElement("xpath", props.getProperty(
+				"IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.Xpath")));
 		wait(1000);
-		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.CNY.Xpath")));
+		clickElement(locateElement("xpath", props.getProperty(
+				"IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.CNY.Xpath")));
 		return this;
 	}
-	
-		
+
 	public IcustAddBaseDenominationmaintenance clickSaveButton() {
 		wait(2000);
-		clickElement(locateElement("xpath",props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Save.Xpath")));
+		clickElement(locateElement("xpath",
+				props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Save.Xpath")));
 		wait(5000);
-        return this;
+		return this;
 	}
 
 }
