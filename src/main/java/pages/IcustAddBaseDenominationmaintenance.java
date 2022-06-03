1196ec9988093daa0a85ae1c501bc140b5fd4b75
shilpa.venkatappa <shilpa.tv@rumango.com>
2022-06-03 13:58:43 +0530

    changes added

---
+++ src/main/java/pages/IcustAddBaseDenominationmaintenance.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+
+import base.IcustSpecificMethods;
+
+public class IcustAddBaseDenominationmaintenance extends IcustSpecificMethods{
+
+	public IcustAddBaseDenominationmaintenance selectBaseDenominationMaintenance() 
+	{
+		clickElement(locateElement("xpath", props.getProperty("Icust.Maintainence.BaseDenominationMaintenance.Xpath")));
+		wait(5000);
+		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.BaseDenominationMaintenance.AddNew.Xpath")));
+		return this;
+	}
+	
+	public IcustAddBaseDenominationmaintenance enterDenomination() {
+		enterValue(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.Bank.AddNew.Denomination.Xpath")), props.getProperty("BankName"));
+		return this;
+	}
+	
+	public IcustAddBaseDenominationmaintenance selectCurrency() {
+		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.Xpath")));
+		wait(1000);
+		clickElement(locateElement("xpath", props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Currency.Dropdown.CNY.Xpath")));
+		return this;
+	}
+	
+		
+	public IcustAddBaseDenominationmaintenance clickSaveButton() {
+		wait(2000);
+		clickElement(locateElement("xpath",props.getProperty("IcustMaintenance.Maintenance.BaseDenominationMaintenance.AddNew.Save.Xpath")));
+		wait(5000);
+        return this;
+	}
+
+}
